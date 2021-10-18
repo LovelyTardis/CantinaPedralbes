66686ee533719d2f4f3ffffdb85ca7543246c833
EricCC <a19ericlecas@inspedralbes.cat>
2021-10-18 14:23:52 +0200

    Test

--- pickup.php
+++ pickup.php
@@ -6,10 +6,33 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="defaultsheet" href="./css/normalize.css">
     <title>Cantina - Ordenar</title>
+    <?php
+    $HTML_products = "";
+    $products = file_get_contents("products.json");
+    $products = json_decode($products,true);
+    $HTML_products = LoadProductsHTML($products);
+    function LoadProductsHTML($productsLoad) : string
+    {
+        $str = "";
+        //
+        $str .="<table>";
+        for ($i=0; $i < count($productsLoad); $i++) { 
+            $str .="<tr><td>id:".$productsLoad[$i]["id"]."</td>".
+            "<td>Nombre:".$productsLoad[$i]["productName"]."</td>".
+            "<td>Precio:".$productsLoad[$i]["price"]."</td></tr>".
+            "<tr><td><button type='button' class='restar'>-</button><span id=".$productsLoad[$i]["id"].">0</span><button type='button' class='sumar'>+</button></td></tr>";
+        }
+        $str .="</table>";
+        //
+        return $str;
+    }
+    
+    ?>
 </head>
 
 <body>
     <h1>PICKUP PAGE (WIP)</h1>
+    <?php echo $HTML_products ?>
     <form method="POST" action="./confirmation.php">
         <button type="submit">Següent</button>
     </form>
