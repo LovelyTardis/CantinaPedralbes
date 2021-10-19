782ac3773431ff0f0fd2e4ad1fa07231140cf300
Marc-0207 <79847498+Marc-0207@users.noreply.github.com>
2021-10-19 12:32:47 +0200

    CSS header

--- header.php
+++ header.php
@@ -1,4 +1,6 @@
+<link rel="stylesheet" href="./css/header.css">
 <header>
+        <img src="assets/images/LogoInspedralbes.png" alt="LogoInstitutPedralbes" class="header-logo">
         <h1 class="head">Institut Pedralbes</h1>
         <hr>
     </header>
