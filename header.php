0edb259251e586659ca11fd103d4310eb3c7331f
a19arnortbri <a19arnortbri@inspedralbes.cat>
2021-10-19 19:09:05 +0200

    Css del header basico

--- header.php
+++ header.php
@@ -1,6 +1,16 @@
 <link rel="stylesheet" href="./css/header.css">
-<header>
-        <img src="assets/images/LogoInspedralbes.png" alt="LogoInstitutPedralbes" class="header-logo">
-        <h1 class="head">Institut Pedralbes</h1>
-        <hr>
-    </header>
+<body>
+    <div class="header">
+        <div class="inner-header">
+            <div class="logo-container">
+                <img src="assets/images/LogoInspedralbes.png" alt="LogoInstitutPedralbes">
+            </div>
+            <ul class="navigation">
+                <a><li>Texto</li></a>
+                <a><li>Texto</li></a>
+                <a><li>Texto</li></a>
+                <a><li>Texto</li></a>
+            </ul>
+        </div>
+    </div>
+</body>
--- readme.txt
+++ readme.txt
@@ -7,3 +7,10 @@ Objectiu:
 Fer una pàgina web funcional per la cantina de l'institut, fent una comanda de menjar.
 
 labs.inspedralbes.cat
+
+
+<header>
+        <img src="assets/images/LogoInspedralbes.png" alt="LogoInstitutPedralbes" class="header-logo">
+        <h1 class="head">Institut Pedralbes</h1>
+        <hr>
+</header>
