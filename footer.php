a41c2a67775c7603d3ecdbb73555746010b71123
Arnau <a19arnortbri@inspedralbes.cat>
2021-10-20 08:46:12 +0200

    Css Footer

--- footer.php
+++ footer.php
@@ -1,4 +1,8 @@
-<footer>
-    <hr>
-    <h5 class="foot"><a href="http://www.institutpedralbes.cat/">Institut Pedralbes</a></h5>
-</footer>
+<link rel="stylesheet" href="./css/footer.css">
+<body>
+    <div class="footer">
+        <div class="inner-footer">
+            <a href="http://www.institutpedralbes.cat/">Copyrigth Institut Pedralbes</a> <u>Web desenvolupada per: Albert Chao, Eric clemente i Arnau Orts.</u>
+        </div>
+    </div>
+</body>
