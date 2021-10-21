af7f723b6e2d5ebadf1b6e19a6ebf39f91fe155d
Albert <a19albchavas@inspedralbes.cat>
2021-10-21 10:41:51 +0200

    Imatges d'alguns productes

--- admin/administration.php
+++ admin/administration.php
@@ -5,9 +5,16 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="defaultsheet" href="./css/normalize.css">
-    <title>Administration</title>
+    <title>Cantina - Admin</title>
+    <?php
+    include 'header.php';
+    ?>
 </head>
 <body>
     <h1>ADMINISTRATION PAGE (WIP)</h1>
+    
+    <?php 
+    include 'footer.php';
+    ?>
 </body>
 </html>
