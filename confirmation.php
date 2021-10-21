e5d2b536c6ca9147534bc83b1007b4003233e242
Arnau <a19arnortbri@inspedralbes.cat>
2021-10-21 10:42:26 +0200

    Css Confirmation

--- confirmation.php
+++ confirmation.php
@@ -8,6 +8,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="defaultsheet" href="./css/normalize.css">
+    <link rel="stylesheet" href="./css/confirmation.css">
     <script src="/SweetAlert2/dist/sweetalert2.all.min.js"></script>
     <title>Cantina - Confirmació</title>
     <?php
@@ -40,20 +41,14 @@
     <?php echo print_r($ticketObjects)?>
     <?php echo $HTML_ticket?>
     <form id="credentialsForm" method="POST" action="./checkout.php">
-    <table>
-        <tr>
-            <td><label for="name">Nom: </label></td>
-            <td><input type="text" name="name" id="name" value="Albert" require></td>
-        </tr>
-        <tr>
-            <td><label for="email">Correu electrònic: </label></td>
-            <td><input type="email" name="email" id="email" value="albert@gmail.com"require></td>
-        </tr>
-        <tr>
-            <td><label for="phone">Telèfon: </label></td>
-            <td><input type="tel" name="phone" id="phone" value="123456789"require></td>
-        </tr>
-    </table>
+    <div  class="grid">
+        <div><label for="name">Nom: </label></div>
+        <div><input type="text" name="name" id="name" value="Albert" require></div>
+        <div><label for="email">Correu electrònic: </label></div>
+        <div><input type="email" name="email" id="email" value="albert@gmail.com"require></div>
+        <div><label for="phone">Telèfon: </label></div>
+        <div><input type="tel" name="phone" id="phone" value="123456789"require></div>
+    </div>
         <button type="button" id="btn-purchase">Confirmar comanda</button>
     </form>
     <div id="ticket"><div id="total-price">0€</div></div>
