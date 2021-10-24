cbf52fab7cbef75ae025e1d40ccffb01807d0980
Albert <72222514+a19albchavas@users.noreply.github.com>
2021-10-24 17:56:38 +0200

    Cambios al header y botones admin
    En el header ahora hay links a inicio y a la zona de administración.
    En la zona de administración ahora hay dos botones que están preparados para saltar a las páginas de adminstration y management.

--- admin/administration.php
+++ admin/administration.php
@@ -4,24 +4,28 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="defaultsheet" href="./css/normalize.css">
+    <link rel="defaultsheet" href="../css/normalize.css">
     <title>Cantina - Admin</title>
     <?php
-    include 'header.php';
+    include '../header.php';
     ?>
     <?php
-    if(isset($_POST['senderJson']))
-    {
-        file_put_contents("../products.json", json_encode(json_decode($_POST['senderJson']), JSON_PRETTY_PRINT));
-    }
-    
+        if(isset($_POST['senderJson']))
+        {
+            file_put_contents("../products.json", json_encode(json_decode($_POST['senderJson']), JSON_PRETTY_PRINT));
+        }
     ?>
 </head>
 <body>
     <h1>ADMINISTRATION PAGE (WIP)</h1>
-    
+    <a>
+        <input type="button" id="product-admin" value="Administrar productes">
+    </a>
+    <a>
+        <input type="button" id="product-manage" value="Modificar productes">
+    </a>
     <?php 
-    include 'footer.php';
+    include '../footer.php';
     ?>
 </body>
 </html>
--- header.php
+++ header.php
@@ -1,13 +1,13 @@
-<link rel="stylesheet" href="./css/header.css">
+<link rel="stylesheet" href="../css/header.css">
 
 <div class="header">
     <div class="inner-header">
         <div class="logo-container">
-            <img src="assets/images/LogoInspedralbes.png" alt="LogoInstitutPedralbes">
+            <img src="../assets/images/LogoInspedralbes.png" alt="LogoInstitutPedralbes">
         </div>
         <ul class="navigation">
-            <a><li>Texto</li></a>
-            <a><li>Texto</li></a>
+            <a href="../index.php"><li>Inici</li></a>
+            <a href="../admin/administration.php"><li>Zona administració</li></a>
             <a><li>Texto</li></a>
             <a><li>Texto</li></a>
         </ul>
