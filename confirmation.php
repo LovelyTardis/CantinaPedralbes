4151b05a0bdd785b8a5f428e1bb35aca1f26e937
Albert <72222514+a19albchavas@users.noreply.github.com>
2021-10-26 14:05:50 +0200

    Update confirmation.php

--- confirmation.php
+++ confirmation.php
@@ -3,7 +3,7 @@
     if(isset($_COOKIE['comanda']))
     {
         //cambiar a cantina cuando se suba
-        header('Location: http://localhost/error.php');
+        header('Location: http://cantina3.alumnes.inspedralbes.cat/error.php');
     }
     if(isset($_POST["basket"]))
     {
@@ -16,7 +16,7 @@
     }
     else
     {
-        header('Location: http://localhost/error.php');
+        header('Location: http://cantina3.alumnes.inspedralbes.cat/error.php');
     }
 ?>
 <!DOCTYPE html>
