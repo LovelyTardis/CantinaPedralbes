8c5be1b64e50cedf8d9f94d9b8820dddad6f8ac5
Arnau <a19arnortbri@inspedralbes.cat>
2021-10-28 11:38:52 +0200

    css

--- admin/ProductManagement.php
+++ admin/ProductManagement.php
@@ -5,6 +5,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="/SweetAlert2/dist/sweetalert2.all.min.js"></script>
+    <link rel="defaultsheet" href="../css/normalize.css">
+    <link rel="stylesheet" href="./css/productmanagment.css">
     <title>Document</title>
     <?php
             $jsonProducts = file_get_contents("../products.json");
@@ -20,8 +22,7 @@
                     $str .= CellProduct($productsLoad[$i]); 
                 }         
     
-                $str .="</div>";
-              
+                $str .="</div>"; 
                 //
                 return $str;
             }
